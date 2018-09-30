a14883aa7662f5a02fb82791bc9ecd715e9e437d
wong <Administrator@WIN10-809221742.mshome.net>
2018-09-30 10:48:17 +0800

    init

---
+++ src/main/java/com/kis/autocopywebsite/AutoCopyWebSiteApplication.java
@@ -0,0 +1,12 @@
+package com.kis.autocopywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCopyWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoCopyWebSiteApplication.class, args);
+    }
+}
